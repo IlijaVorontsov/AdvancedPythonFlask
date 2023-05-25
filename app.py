d8a38e204b2198dbd7103fd390ac62d6f562d555
IlijaVorontsov <32547475+IlijaVorontsov@users.noreply.github.com>
2023-05-25 18:05:07 +0200

    Added list_users, email functionality

--- app.py
+++ app.py
@@ -1,33 +1,44 @@
 from flask import Flask, url_for, render_template, request, redirect
 from markupsafe import escape
+from mail import *
 
 app = Flask(__name__)
 
-users = {}
-
 class User:
-    def __init__(self, username, password, email):
+    def __init__(self, username, email, send = False):
         self.username = username
         self.email = email
-        self.__password = password
+        self.__password = generate_password()
+        if send:
+            send_register_mail(self.email, self.username, self.__password)
+    
     def check_password(self, password):
         return self.__password == password
+    
+    def reset_password(self):
+        self.__password = generate_password()
+        send_reset_mail(self.email, self.username, self.__password)
+
+users = {}
+users['ali'] = User('ali', 'ali@hotmail.com')
+users['hans'] = User('hans', 'hans@gmail.com')
+users['peter'] = User('peter', 'peter@gmx.at')
+users['max'] = User('max', 'max@a1.at')
+
+
     
 @app.route('/')
 @app.route('/register', methods=['GET','POST'])
 def register():
     if request.method == 'POST':
         username = request.form['uname']
-        password = request.form['psw']
         email = request.form['email']
-        if password != request.form['psw-repeat']:
-            return render_template('register.html', error='Passwords do not match')
 
         if username in users:
-            return render_template('register.html', error='User already exists')
+            return render_template('register.html', error='User already exists!')
         else:
-            users[username] = User(username, password, email)
-            return render_template('register.html', error=f'User {username} created')
+            users[username] = User(username, email)
+            return render_template('register.html', success=f'User {username} created!')
     return render_template('register.html')
 
 @app.route('/login', methods=['GET','POST'])
@@ -45,7 +56,19 @@ def login():
             return 'User not found'
     return render_template('login.html')
 
-@app.route('/users')
+@app.route('/reset', methods=['GET','POST'])
+def reset():
+    if request.method == 'POST':
+        username = request.form['uname']
+        if username in users and users[username].email == request.form['email']:
+            user = users[username]
+            user.reset_password()
+            return redirect(url_for('login'))
+        else:
+            return render_template('reset.html', error='User not found or email wrong!')
+    return render_template('reset.html')
+
+@app.route('/list_users')
 def list_users():
     return render_template('users.html', users=users)
 
---
+++ mail.py
@@ -0,0 +1,61 @@
+# script to send emails
+from email.message import EmailMessage
+import ssl
+import smtplib
+import random
+
+
+email = 'advancedpythoncourse@gmail.com'
+password = 'nilhbosaifsqcgug'
+email_to = 'ali.170403@hotmail.com'
+
+
+def generate_password():
+    letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+    numbers = '0123456789'
+    password = ''
+    for _ in range(4):
+        for _ in range(5):
+            password += random.choice(letters + numbers)
+        password += '-'
+    password = password[:-1]
+    return password
+
+def send_register_mail(email_to, username, password):
+    email_msg = EmailMessage()
+    email_msg['From'] = email
+    email_msg['To'] = email_to
+    email_msg['Subject'] = 'Hello from Python!'
+
+    body = f'''
+    Willkommen bei unserem Kurs!
+    Dein Benutzername ist: {username}
+    Dein Passwort ist: {password}
+    '''
+
+    email_msg.set_content(body)
+
+    send_mail(email_to, email_msg)
+
+
+def send_reset_mail(email_to, username, password):
+    email_msg = EmailMessage()
+    email_msg['From'] = email
+    email_msg['To'] = email_to
+    email_msg['Subject'] = 'Hello from Python!'
+
+    body = f'''
+    Hallo {username}!
+    Dein neues Passwort ist: {password}
+    '''
+
+    email_msg.set_content(body)
+
+    send_mail(email_to, email_msg)
+
+def send_mail(email_to, email_msg):
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+        smtp.login(email, password)
+        smtp.sendmail(email, email_to, email_msg.as_string())
+
